2ac14c9821e010118de915c103df7f5075b1dc9b
MDN <mark.denardo@gmail.com>
2026-04-07 19:11:52 -0400

    Add assembly.js for audio-visual processing

---
+++ assembly.js
@@ -0,0 +1,26 @@
+a = []
+
+for (i = 0; i < 100; i++) {
+	a.push(i)
+}
+
+f = () => a.fft[0]
+
+osc(f, f, f)
+	.pixelate(a.smooth(), () => time)
+	.scale(0.01, 0.1)
+	.diff(src(o0)
+		.scale(0.9))
+	.out(o0)
+
+src(o0)
+	.blend(shape(3)
+		.pixelate(f / 50, f / 50))
+	.out(o1)
+render(o1)
+
+src(o1)
+	.rotate()
+	.out(o2)
+
+render(o2)
